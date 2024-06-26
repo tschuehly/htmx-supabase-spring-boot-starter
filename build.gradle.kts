418c2421d851cb8fb7cd635947d4cdcb2bef74fd
Thomas Schuehly <thomas.schuehly@outlook.com>
2024-06-26 09:14:14 +0200

    Update project version and add new security class
    The project version in build.gradle.kts was updated to 0.3.5-SNAPSHOT. Additionally, a new SupabaseSecurityContextHolder.kt file was added. This file, included in the de.tschuehly.htmx.spring.supabase.auth.security package, introduces a static method that retrieves authenticated users.

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ plugins {
 }
 
 group = "de.tschuehly"
-version = "0.3.3"
+version = "0.3.5-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
---
+++ src/main/kotlin/de/tschuehly/htmx/spring/supabase/auth/security/SupabaseSecurityContextHolder.kt
@@ -0,0 +1,18 @@
+package de.tschuehly.htmx.spring.supabase.auth.security
+
+import de.tschuehly.htmx.spring.supabase.auth.types.SupabaseUser
+import org.springframework.security.authentication.AnonymousAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+
+class SupabaseSecurityContextHolder {
+    companion object {
+        @JvmStatic
+        fun getAuthenticatedUser(): SupabaseUser? {
+            val authentication = SecurityContextHolder.getContext().authentication
+            if (authentication !is AnonymousAuthenticationToken) {
+                return (authentication as SupabaseAuthenticationToken).principal
+            }
+            return null
+        }
+    }
+}
