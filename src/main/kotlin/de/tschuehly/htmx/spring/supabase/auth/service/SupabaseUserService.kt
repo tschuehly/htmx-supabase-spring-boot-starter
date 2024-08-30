12a885e62b2564d487bb28260460ded8168695b4
Thor Weinreich <tweinreich@users.noreply.github.com>
2024-08-30 21:52:49 +0400

    - enroll, verify and unenroll TOTP - add meta data to user on sign up - invite user by email

--- src/main/kotlin/de/tschuehly/htmx/spring/supabase/auth/service/SupabaseUserService.kt
+++ src/main/kotlin/de/tschuehly/htmx/spring/supabase/auth/service/SupabaseUserService.kt
@@ -1,5 +1,8 @@
 package de.tschuehly.htmx.spring.supabase.auth.service
 
+import io.github.jan.supabase.gotrue.user.UserInfo
+import io.github.jan.supabase.gotrue.user.UserMfaFactor
+import io.github.jan.supabase.gotrue.user.UserSession
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 
@@ -17,4 +20,32 @@ interface SupabaseUserService {
     fun sendPasswordRecoveryEmail(email: String)
     fun updatePassword(request: HttpServletRequest, password: String)
     fun sendOtp(email: String)
+    fun signUpWithEmailAndMeta(
+        request: HttpServletRequest,
+        email: String,
+        password: String,
+        meta: Map<String, String>,
+        response: HttpServletResponse
+    ): UserInfo?
+    fun signUpWithEmailAndMeta(
+        serviceJwt: String,
+        email: String,
+        password: String,
+        meta: Map<String, String>,
+        response: HttpServletResponse
+    ): UserInfo?
+
+    fun inviteUserByEmail(
+        serviceJwt: String,
+        email: String,
+        redirectUrl: String,
+        meta: Map<String, String>,
+        response: HttpServletResponse
+    )
+
+    fun retrieveMFAFactorsForCurrentUser(request: HttpServletRequest): List<UserMfaFactor>?
+    fun enrollSecondFactor(request: HttpServletRequest, issuer: String, deviceName: String): Map<String, String>?
+    fun unenrollMfaFactor(request: HttpServletRequest, factorId: String)
+    fun createAndVerifyMFAChallenge(request: HttpServletRequest, factorId: String, code: String): UserSession?
+    fun loggedInUsingMfa(request: HttpServletRequest): Boolean
 }
