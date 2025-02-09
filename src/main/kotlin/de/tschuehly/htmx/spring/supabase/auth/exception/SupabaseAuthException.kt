51671b5494a2646d494294a8dda6473dd9566675
tschuehly <tschuehly@outlook.com>
2025-02-09 21:38:14 +0100

    Improve exc logging

--- src/main/kotlin/de/tschuehly/htmx/spring/supabase/auth/exception/SupabaseAuthException.kt
+++ src/main/kotlin/de/tschuehly/htmx/spring/supabase/auth/exception/SupabaseAuthException.kt
@@ -3,8 +3,8 @@ package de.tschuehly.htmx.spring.supabase.auth.exception
 import io.github.jan.supabase.auth.exception.AuthRestException
 
 
-class SupabaseAuthException(exc: AuthRestException) :
-    Exception("Supabase failed: ${exc.error} with message: ${exc.message}, code: ${exc.errorCode}", exc) {
+class SupabaseAuthException(exc: AuthRestException, email: String) :
+    Exception("Supabase failed for email:$email - ${exc.error} with message: ${exc.message}, code: ${exc.errorCode}", exc) {
     val errorCode = exc.errorCode
     val error: String = exc.error
 }
